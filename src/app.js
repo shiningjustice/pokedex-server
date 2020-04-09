01ca2b11dfaa6a31380b772736cedf659c80df94
Phoebe Law <phoebebasilio@gmail.com>
2020-04-09 12:59:25 -0700

    connects auth to app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const userRouter = require('./user/user-router');
+const authRouter = require('./auth/auth-router');
 
 const app = express(); 
 
@@ -21,6 +22,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
 app.use(function errorHandler(error, req, res, next) {
