3a6dac7fcb944c57fb34a4099962568bd99bd12f
Phoebe Law <phoebebasilio@gmail.com>
2020-04-11 16:45:14 -0700

    adds API token to config.js

--- src/config.js
+++ src/config.js
@@ -7,4 +7,5 @@ module.exports = {
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://admin@localhost/pokedex-test',
   JWT_SECRET: process.env.JWT_SECRET || 'changeme',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  API_TOKEN: process.env.API_TOKEN
 }
